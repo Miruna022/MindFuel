96f4605ec0be65ec6c21239126ce92cdee5a72f9
Miruna022 <maria.mesarosiu05@e-uvt.ro>
2025-03-15 11:30:03 +0200

    Add functionality to answer screen

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { StartScreen } from './screens/startScreen';
 import { useFonts } from 'expo-font';
+import {AnswerScreen} from "./screens/answerScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StartScreen/>
+      <AnswerScreen/>
     </View>
   )
 }
